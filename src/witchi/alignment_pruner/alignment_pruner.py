changed calculation of significant taxa to be corrected for multiple testing

src/witchi/alignment_pruner/alignment_pruner.py
@@ -116,8 +116,8 @@ class AlignmentPruner:
                 score_dict["before_permuted"] = permutated_per_row_chi2
                 score_dict["before_real"] = per_row_chi2
 
-            significant_count = np.sum(per_row_chi2 > self.significance_level)
-            significant_count_permutation = np.sum(per_row_chi2 > upper_threshold)
+            pseudo_pvalues = chi_square_calculator.calc_pseudo_pvalue(per_row_chi2, permutated_per_row_chi2)
+            significant_count = len(pseudo_pvalues >= 0.05)
 
             if per_row_chi2_median <= upper_box_threshold:
                 if self.pruning_algorithm == 'wasserstein':
@@ -145,7 +145,7 @@ class AlignmentPruner:
             count_rows_array = chi_square_calculator.calculate_row_counts(alignment_array)
             original_indices = [i for j, i in enumerate(original_indices) if j not in top_n_indices]
             iteration += 1
-            print(f"Columns removed: {removed_columns_count}, {(removed_columns_count / alignment_size) * 100:.2f}% Significant rows: {significant_count} Significant rows permutation: {significant_count_permutation} Mean z-score: {(np.mean(per_row_chi2) - mean_perm_chi2) / sd_perm_chi2:.2f} q95 z-score: {(upper_chi_quantile - upper_threshold) / (upper_threshold - mean_perm_chi2):.2f}")
+            print(f"Columns removed: {removed_columns_count}, {(removed_columns_count / alignment_size) * 100:.2f}% | Biased taxa permutation: {significant_count} | Mean z-score: {(np.mean(per_row_chi2) - mean_perm_chi2) / sd_perm_chi2:.2f} | q95 z-score: {(upper_chi_quantile - upper_threshold) / (upper_threshold - mean_perm_chi2):.2f}")
 
         score_dict["after_real"] = per_row_chi2
 